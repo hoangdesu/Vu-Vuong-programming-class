6d8c83980bcaee012598e48d2dc761c42b22eb43
hoangdesu <hoangdesu@gmail.com>
2024-10-11 12:08:06 +0700

    nested for loops

---
+++ ProgrammingBootcamp1/LearnJava/src/main/java/Oct11_NestedLoops.java
@@ -0,0 +1,89 @@
+public class Oct11_NestedLoops {
+    public static void main(String[] args) throws InterruptedException {
+//        for (int i = 1; i < 4; i++) {
+//            for (int j = 7; j < 11; j++) {
+//                System.out.printf("i = %d | j = %d\n", i, j);
+//            }
+//        }
+
+//        for (int hour = 0; hour < 24; hour++) {
+//            for (int minute = 0; minute < 60; minute++) {
+//                for (int second = 0; second < 60; second++) {
+//                    System.out.printf("%d:%d:%d\n", hour, minute, second);
+//                    Thread.sleep(1000);
+//                }
+//            }
+//        }
+
+//        exercise 1
+//        *
+//        **
+//        ***
+//        ****
+//        ******
+
+//        void printStarPyramid(6)
+//        *
+//        **
+//        ***
+//        ****
+//        ******
+//        *******
+
+//        exercise 2:
+//        printReversedNumberPyramid(5)
+//        1 2 3 4 5
+//        1 2 3 4
+//        1 2 3
+//        1 2
+//        1
+
+
+//        exercise3: printPyramid(5)
+//            *
+//           ***
+//          *****
+//         *******
+//        *********
+//
+//        line        stars       spaces
+//        1           1           4
+//        2           3           3
+//        3           5           2
+//        4           7           1
+//        5           9           0
+//        ...
+//        16          31
+//        n           n*2-1
+
+//        stars = line * 2 - 1
+
+//        Homework:
+//        printDiamond(5)
+//            *
+//           ***
+//          *****
+//           ***
+//            *
+
+//        printDiamond(6)
+//            *
+//           ***
+//          *****
+//          *****
+//           ***
+//            *
+
+
+//        printChristmasTree(5)
+
+//            *
+//           ***
+//          *****
+//         *******
+//        *********
+//           | |
+//           |_|
+
+    }
+}

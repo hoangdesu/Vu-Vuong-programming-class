d3822c3ce6791b24989e44fe57e07b6d038c6c4a
hoangdesu <hoangdesu@gmail.com>
2026-01-07 15:44:06 +0700

    JS vs TS, client-side vs server-side, JS engines, browser differences

---
+++ WebDev/frontend/jan7-js/hey.ts
@@ -0,0 +1,5 @@
+let aa = 1;
+
+aa = 'hey';
+
+console.log(aa)
---
+++ WebDev/frontend/jan7-js/hi.js
@@ -0,0 +1,26 @@
+// Old version, not recommended
+
+var a = 1
+a = 'hi'
+
+a -= 10
+
+console.log(a); // NaN
+
+// ES6: recommend using 'let'
+let b = 2
+b = '=))'
+b += 3
+
+console.log(b);
+
+console.log(1 + '2'); // '12'
+
+console.log(10 - '3'); // 7
+
+const bestGame = 'e33';
+// bestGame = 'hades 2' // error
+
+console.log(bestGame);
+
+// transpiled
